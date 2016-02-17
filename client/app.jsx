0c2e40dc9da7397f0e1e469e33f57e4e6fabc965
John Rodney <jrodney@poeticsystems.com>
2016-02-16 22:32:11 -0600

    see if travis is enfocing const vs let rules

--- client/app.jsx
+++ client/app.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 
 // define and export our Layout component
-export const Layout = ({ content }) => (
-    <div>
-        <h1>My App</h1>
-        <hr />
-        <div>{content}</div>
-    </div>
+export let Layout = ({ content }) => (
+  <div>
+    <h1>My App</h1>
+    <hr />
+    <div>{content}</div>
+  </div>
 );
