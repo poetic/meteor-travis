de0cc5b2e09b0d36112c04d49bcd4bc9526c3d4f
John Rodney <jrodney@poeticsystems.com>
2016-02-16 22:40:21 -0600

    adds const to welcome.jsx

--- client/app.jsx
+++ client/app.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 // define and export our Layout component
-export let Layout = ({ content }) => (
+export const Layout = ({ content }) => (
   <div>
     <h1>My App</h1>
     <hr />
